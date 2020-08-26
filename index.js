1c5ee49ddc0600c7b774e9bba599a4773b12ab4a
brianwlego <legobw@gmail.com>
2020-08-25 23:25:18 -0400

    Finished Deliverables

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function renderPlayer(player) {
 
   // append the element to the container
   playerContainer.append(playerDiv)
+  addNewLike()
 }
 
 // for each player in the array, render to the DOM
@@ -39,8 +40,42 @@ function toggleColor(element) {
     element.style.color = "red"
   }
 }
-
+const header = document.querySelector("h1#header")
+header.addEventListener('mouseover', function(e){
+  e.target.style.cursor = "pointer"
+})
+header.addEventListener('click', function(e){
+  toggleColor(header)
+})
 
 /***** Deliverable 2 *****/
+const form = document.getElementById('new-player-form')
+form.addEventListener('submit', function(e){
+  e.preventDefault()
+  const inputNum = form.number.value, inputNickname = form.nickname.value, inputName = form.name.value, inputPhoto = form.photo.value
+  
+  const newPlayer = {number: inputNum, name: inputName, nickname: inputNickname, photo: inputPhoto, likes: 0}
+  
+  renderPlayer(newPlayer)
+
+})
 
 /***** Deliverable 3 *****/
+function addNewLike(){
+  const likeBtn = document.getElementsByClassName('like-button')
+  const array = Array.from(likeBtn)
+  array.forEach(function(like){
+      like.addEventListener('click', function(e){
+      let likeNum = e.target.parentElement.getElementsByClassName('likes')[0].innerHTML
+      likeNum.replace("likes ", "")
+      likeNum = parseInt(likeNum, 10)
+      e.target.parentElement.getElementsByClassName('likes')[0].innerHTML = (likeNum + 1)
+    })
+  })
+}
+
+// function like(player){
+//   const likeBtn = document.querySelector(`div.like-button[data-number='${player.number}'`)
+
+
+// }
